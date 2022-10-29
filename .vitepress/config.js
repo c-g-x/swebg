f68d350a44ca5013f15423c95b081ff85cdf22d7
chenguixin <591764955@qq.com>
2022-10-29 21:51:01 +0800

    ci:change config

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -2,5 +2,9 @@ export default {
     title: 'WEB 框架小组',
     description: 'WEB 框架小组文档',
     lang: 'zh-CN',
-    base: '/swebg/'
+    base: '/swebg/',
+    markdown: {
+        theme: 'material-palenight',
+        lineNumbers: true
+    }
 };
--- config.js
+++
@@ -1,6 +0,0 @@
-export default {
-    title: 'WEB 框架小组',
-    description: 'WEB 框架小组文档',
-    lang: 'zh-CN',
-    base: '/swebg/'
-};
