f971c510e32f4ac3028d6eafa9720e4ee6f5054f
chenguixin <591764955@qq.com>
2022-11-02 09:36:47 +0800

    docs:add sidebar

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -6,5 +6,26 @@ export default {
     markdown: {
         theme: 'material-palenight',
         lineNumbers: true
+    },
+    themeConfig: {
+        sidebar: [
+            {
+                text: '规范合集',
+                items: [
+                    {
+                        text: '代码提交规范',
+                        link: '/spec/code-spec.md'
+                    },
+                    {
+                        text: '组件发布流程',
+                        link: '/spec/component-publish-spec/index.md'
+                    },
+                    {
+                        text: '脚手架升级流程',
+                        link: '/spec/cli-update/index.md'
+                    }
+                ]
+            }
+        ]
     }
 };
--- index.md
+++ index.md
@@ -1,6 +1,1 @@
 # 首页
-
-1. [规范合集](/spec/index.md)
-    1. [代码提交规范](/spec/code-spec.md)
-    2. [组件发布流程](/spec/component-publish-spec/index.md)
-    3. [脚手架升级流程](/spec/cli-update/index.md)
